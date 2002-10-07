a4b92d44baee92cd65aced154ad5ef9ab4c2c772
fujihara <fujihara@cvs.orca.med.or.jp>
2002-10-07 11:44:17 +0900

    ＨＣＡＬＬ２．ｓｈｍｏｎｐｅ対応

--- scripts/daily/HCALL2.sh
+++ scripts/daily/HCALL2.sh
@@ -7,7 +7,7 @@
 	echo	`date`
 	echo	$1
 
-ruby /usr/local/orca/scripts/daily/print_parent2.rb "temp.tmp" "ruby /usr/local/orca/scripts/daily/print-data.rb " $1 
+ruby /usr/local/orca/scripts/daily/print_parent2.rb "/var/tmp/temp.tmp" "ruby /usr/local/orca/scripts/daily/print-data.rb " $1 
 
 	rm -f $1
 	echo	`date`
