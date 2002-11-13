43737a8fe04aac4142821b70a53154525fad2a22
takeda <takeda@cvs.orca.med.or.jp>
2002-11-13 17:14:12 +0900

    woody対応

--- scripts/daily/print_parent2.rb
+++ scripts/daily/print_parent2.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 
 # 文字列の改行区切りで配列にして、それを引数のプログラムに渡す処理
 # (2002/03/07 竹田氏追加依頼)
@@ -9,6 +9,8 @@
 #   woody版では、site-libのdia/redファイルも見なければならないので、それに関する対応
 #   基準ディレクトリを追加することで対応
 #   dataファイルフォーマットを修正(LP名のあとに、site区分を追加)
+# ディレクトリ位置対応 (2002/11/13)
+#   コマンドライン引数3〜6のディレクトリ位置情報を使用するように修正
 
 # ※複数のプロセスの実行はできない
 
@@ -16,7 +18,11 @@
 # 引数指定方法
 # 引数1 = 一時ファイル名
 # 引数2 = 起動プログラム
-# 引数3 = dataファイル
+# 引数3 = 日医標準レセプトformディレクトリ
+# 引数4 = 日医標準レセプトrecordディレクトリ
+# 引数5 = 日医標準レセプトsite固有formディレクトリ
+# 引数6 = 日医標準レセプトsite固有recordディレクトリ
+# 引数7 = dataファイル
 
 # dataファイルフォーマット
 # +------------------+------------+---------------+--------------------------+
@@ -28,10 +34,6 @@
 #      2 = site-libのカスタマイズ帳票
 #
 # ※LP名は空白の場合、子プロセスの引数を省略する
-
-# 引数3 = diaで使うXML形式帳票定義ファイル
-# 引数4 = defファイル
-# 引数6 = LP名(省略可)
 
 
 
@@ -54,31 +56,31 @@ end
 
 if (help_flg != 0)
 	puts 'このスクリプトは、改行で区切って、引数のプログラムに渡すプログラムです'
-	puts $0 + ' <temp> <exec> <data>'
-	puts '<temp>    = 一時ファイル名'
-	puts '<exec>    = 起動プログラム'
-	puts '<data>    = dataファイル'
+	puts $0 + ' <temp> <exec> <form> <record> <site-form> <site-record> <data>'
+	puts '<temp>        = 一時ファイル名'
+	puts '<exec>        = 起動プログラム'
+	puts '<form>        = 日医標準レセプトformディレクトリ'
+	puts '<record>      = 日医標準レセプトrecordディレクトリ'
+	puts '<site-form>   = 日医標準レセプトsite固有formディレクトリ'
+	puts '<site-record> = 日医標準レセプトsite固有recordディレクトリ'
+	puts '<data>        = dataファイル'
 	exit 0
 end
 
 
+std_form = ''; std_record = ''; site_form = ''; site_record = ''
+
 # 定数の指定(最後に、かならず「/」を付けた形にしてください)
-# 修正開始 (2002/10/16)
-BASE_DIR1 = ARGV[2] + '/'		# ORCA標準
-BASE_DIR2 = ARGV[3] + '/'		# site-lib
+std_form = ARGV[2] + '/'   		# 日レセ標準formディレクトリ
+std_record = ARGV[3] + '/' 		# 日レセ標準recordディレクトリ
+site_form = ARGV[4] + '/'  		# 日レセsite固有formディレクトリ
+site_record = ARGV[5] + '/'		# 日レセsite固有recordディレクトリ
 
-
-DIA_DIR = 'form/'
-DEF_DIR = 'record/'
-# 追加開始 (2002/09/17)
-RED_DIR = 'form/'
 
 # .redからpsファイルに変換する際に使用するプログラム
 RED_EXEC = 'red2ps'
 # .redファイルで、LP名を省略された際に使用するプリンタ名
 DEFAULT_LP = 'lp1'
-# 追加終了 (2002/09/17)
-# 修正終了 (2002/10/16)
 
 
 
@@ -86,11 +88,9 @@ DEFAULT_LP = 'lp1'
 # 領域の初期化
 
 # ファイル名領域
-# 修正開始 (2002/10/16)
 temp_file = ''; exec_file = ''; dia_file = ''; def_file = ''; data_file = ''; lp_name = ''
 temp_file2 = ''; exec_file2 = ''; dia_file2 = ''; def_file2 = ''; data_file2 = ''; lp_name2 = ''
 site_flag = ''; base_dir = ''
-# 修正終了 (2002/10/16)
 # 文字列領域
 word1 = ''; word2 = []; word3 = ''
 # exec用文字列領域
@@ -107,7 +107,7 @@ d2_len = 0
 # 引数のセット
 
 # 引数の数チェック
-for li_cnt1 in 1..2 do
+for li_cnt1 in 1..6 do
 	if ARGV[li_cnt1] == nil
 		puts '引数の数が足りません'
 		exit 1
@@ -118,7 +118,7 @@ end
 # 引数の領域から、文字列の取得
 temp_file = String(ARGV[0])		# 一時ファイル
 exec_file = String(ARGV[1])		# 起動プログラムファイル
-data_file = String(ARGV[4])		# dataファイル名
+data_file = String(ARGV[6])		# dataファイル名
 
 
 # ============================================================
@@ -228,36 +228,33 @@ word2.each do |d2|
 
 
 	# 先頭30バイトは、.diaと.defファイルのファイル名部分
-	# あるいは、.redファイルの拡張子を含むファイル名 (2002/09/17追加仕様)
+	# あるいは、.redファイルの拡張子を含むファイル名
 	dia_file = ''
 	def_file = ''
-# 追加開始 (2002/09/17)
 	red_file = ''
-# 追加終了 (2002/09/17)
 	ls_w1 = ''	# 一時用文字列領域
 	ls_w1 = d2[0, 30].strip	# ファイル名を後ろの空白を除いて取得
-# 追加開始 (2002/10/16)
-	site_flag = d2[50, 1].strip
-	case	site_flag
-	when	'1'
-		base_dir = BASE_DIR1
-	when	'2'
-		base_dir = BASE_DIR2
-	else
-		base_dir = BASE_DIR1
-	end
-# 追加終了 (2002/10/16)
-# 追加開始 (2002/09/17)
+	site_flag = d2[50, 1].strip		# site固有判定フラグの取得
 	if ls_w1 =~ /.red$/
 		# .redファイルである
-		red_file = base_dir + RED_DIR + ls_w1		# Redファイル名のセット(2002/10/16修正)
+
+		# Redファイル名のセット
+		case	site_flag
+		when	'1'
+			red_file = std_form + ls_w1
+		when	'2'
+			red_file = site_form + ls_w1
+		else
+			red_file = std_form + ls_w1
+		end
+
 		lp_name = d2[30, 20].strip	# LP名のセット
-		word3 = d2[51, (d2_len - 51)]	# 一時ファイルへ出力する内容のセット(2002/10/16修正)
+		word3 = d2[51, (d2_len - 51)]	# 一時ファイルへ出力する内容のセット
 
 		# 一時ファイルへの書き込み
 		open(temp_file, "w") do |fp|
 			fp.print word3
-			fp.print ' ' * 10000		# 2002/03/07 竹田氏対応依頼
+			fp.print ' ' * 10000
 		end
 
 # デバッグ用の表示
@@ -296,17 +293,28 @@ word2.each do |d2|
 
 	else
 		# .redファイル以外である
-# 追加終了(対応するendまで、インデントを追加している) (2002/09/17)
-		dia_file = base_dir + DIA_DIR + ls_w1 + '.dia'		# Diaファイル名のセット(2002/10/16修正)
-		def_file = base_dir + DEF_DIR + ls_w1 + '.def'		# Defファイル名のセット(2002/10/16修正)
+
+		# Dia・defファイル名のセット
+		case	site_flag
+		when	'1'
+			dia_file = std_form + ls_w1 + '.dia'
+			def_file = std_record + ls_w1 + '.def'
+		when	'2'
+			dia_file = site_form + ls_w1 + '.dia'
+			def_file = site_record + ls_w1 + '.def'
+		else
+			dia_file = std_form + ls_w1 + '.dia'
+			def_file = std_record + ls_w1 + '.def'
+		end
+
 		lp_name = d2[30, 20].strip	# LP名のセット
-		word3 = d2[51, (d2_len - 51)]	# 一時ファイルへ出力する内容のセット(2002/10/16修正)
+		word3 = d2[51, (d2_len - 51)]	# 一時ファイルへ出力する内容のセット
 
 
 		# 一時ファイルへの書き込み
 		open(temp_file, "w") do |fp|
 			fp.print word3
-			fp.print ' ' * 10000		# 2002/03/07 竹田氏対応依頼
+			fp.print ' ' * 10000
 		end
 # デバッグ用の表示
 		case	word3
@@ -323,14 +331,14 @@ word2.each do |d2|
 		end
 		w_exec = exec_file + ' ' + dia_file + ' ' + def_file + ' ' + temp_file
 #		w_exec = exec_file + ' ' + dia_file2 + ' ' + def_file2 + ' ' + temp_file2
-		# LP名が指定されていたら、引数に追加(2002/03/07 竹田氏作成依頼)
+		# LP名が指定されていたら、引数に追加
 		if lp_name != ''
 			w_exec = w_exec + ' ' + lp_name
 		end
 # デバッグ用の表示
 #		puts w_exec
 # **
-		# 実行前メッセージ出力(2002/03/07 竹田氏作成依頼)
+		# 実行前メッセージ出力
 		puts	'Print Start [' + String(li_cnt1) + ']'
 		# プログラムの実行
 		pid = fork do
@@ -340,9 +348,7 @@ word2.each do |d2|
 		# 実行したプログラムが終わるまで待つ(引数の２番目は、1.4でのエラー回避のため)
 		Process.waitpid(pid, 0)
 
-# 追加開始 (2002/09/17)
 	end
-# 追加終了 (2002/09/17)
 end
 
 
