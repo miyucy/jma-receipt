ba1754913d4614cbc80df0444c84fba3bc2afd19
takeda <takeda@cvs.orca.med.or.jp>
2005-01-26 09:55:16 +0900

    受診履歴インデックス一部変更

--- sql/orcadbup_jyurrk_2.5.0-1.sql
+++ sql/orcadbup_jyurrk_2.5.0-1.sql
@@ -1,4 +1,4 @@
 
-CREATE INDEX idx_jyurrk_sryymd ON tbl_jyurrk USING btree (hospid, sryymd, nyugaikbn, hknkbn);
+CREATE INDEX idx_jyurrk_sryymd ON tbl_jyurrk USING btree (hospid, sryymd, nyugaikbn);
 
 
