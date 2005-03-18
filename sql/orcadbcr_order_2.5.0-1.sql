c8837c01c264c11c1f9cd83c78c5e8b4d288f1ad
ota <ota@cvs.orca.med.or.jp>
2005-03-18 10:51:26 +0900

    入院オーダー対応

--- sql/orcadbcr_order_2.5.0-1.sql
+++ sql/orcadbcr_order_2.5.0-1.sql
@@ -3,8 +3,8 @@
 --                                --
 -- 入院オーダー情報       --
 --                                --
--- Table Name  : TBL_ORDER       --
--- Create Date : 2004/10/14       --
+-- Table Name  : TBL_ORDER        --
+-- Create Date : 2005/03/15       --
 --                                --
 
 create table TBL_ORDER (
@@ -16,33 +16,34 @@ create table TBL_ORDER (
 	SRYKA				char(2) 		not null,
 	SRYYMD				char(8) 		not null,
 	HKNCOMBI			numeric(4,0) 	not null,
+	DRCD				char(5)			not null,
+	ORDERID				char(4)			not null,
 	ORDERNUM			numeric(2,0) 	not null,
-	RENNUM				numeric(2,0) 	not null,
+	ITEMNUM				numeric(2,0) 	not null,
 	UKEYMD   			char(8) 		not null,
 	UKEHMS   			char(6) 		not null,
-	DRCD				char(5),
-	STATUS				char(1),
 	ERRCD1				char(4),
 	ERRCD2				char(4),
-	ZAICD				char(2),
-	SRYSYUKBN			char(3),
-	IDOKBN				char(2),
-	SRYSKBKBN			char(5),
-	SRYCD				char(9),
-	SRYSURYO			numeric(8,3),
-	SRYKAISU			numeric(3,0),
-	ZAIKAISU			numeric(8,0),
-	ERRCD_ZAI			char(4),
-	ERRCD_ITEM			char(4),
-	CREKBN				char(1),
-	KARTE_KEY_STS		char(1),
+	ITEMID				char(5),
+	ITEM				varchar(10),
 	REMARKS				varchar(80),
+	ERRCD_ORDER			char(4),
+	ERRCD_ITEM			char(4),
+	STATUS_ORDER		char(1),
+	STATUS_ITEM			char(1),
+	MODIFY				char(1),
+	KEYGENERATE			char(1),
+	RRKNUM				numeric(3,0),
+	RRKEDANUM			numeric(3,0),
 	TERMID   			varchar(16),
 	OPID     			varchar(16),
 	CREYMD   			char(8),
 	UPYMD    			char(8),
 	UPHMS				char(6),
 constraint TBL_ORDER_primary_key primary key (HOSPID,KARTE_KEY,NYUGAIKBN,PTNUM,PTID,SRYKA,SRYYMD,HKNCOMBI,
-ORDERNUM,RENNUM,UKEYMD,UKEHMS));
+DRCD,ORDERID,ORDERNUM,ITEMNUM,UKEYMD,UKEHMS));
 
 create index idx_order_ukeymd ON tbl_order USING btree (ukeymd);
+create index idx_order_sryymd ON tbl_order USING btree (sryymd);
+create index idx_order_ptid   ON tbl_order USING btree (ptid);
+create index idx_order_sryymd ON tbl_order USING btree (sryymd);
