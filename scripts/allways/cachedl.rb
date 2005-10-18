ef3cab1f12942143bf6cd464152891e3e0e583ed
tosihiko <tosihiko@cvs.orca.med.or.jp>
2005-10-18 12:45:12 +0900

    /var/lib/jma-receipt/cache/patch -> /var/lib/jma-receipt/patch

--- scripts/allways/cachedl.rb
+++ scripts/allways/cachedl.rb
@@ -3,33 +3,33 @@
 require "digest/md5"
 require "readline"
 
-  package_file = ARGV[0]  # ex. patch2.6.0.gz
-  save_dir = ARGV[1]+"/"  # ex. /var/lib/jma-receipt/cache/patch/
-  package_url = "ftp://"+ARGV[2]+"/"
+package_file = ARGV[0]  # ex. patch2.6.0.gz
+save_dir = ARGV[1]+"/"  # ex. /var/lib/jma-receipt/patch/
+package_url = "ftp://"+ARGV[2]+"/"
   
-  if File.exist?(save_dir+package_file)
-    File.delete(save_dir+package_file)
-  end
-  if File.exist?(save_dir+package_file.gsub(/\.gz$/, ""))
-    File.delete(save_dir+package_file.gsub(/\.gz$/, ""))
-  end
+if File.exist?(save_dir+package_file)
+   File.delete(save_dir+package_file)
+end
+if File.exist?(save_dir+package_file.gsub(/\.gz$/, ""))
+   File.delete(save_dir+package_file.gsub(/\.gz$/, ""))
+end
 
-  # ftp.orca.med.or.jp より package.gz をダウンロード
-  `wget -q -P #{save_dir} #{package_url+package_file}`
-  `gunzip -f #{save_dir+package_file}`
+# ftp.orca.med.or.jp よりダウンロード
+`wget -q -P #{save_dir} #{package_url+package_file}`
+`gunzip -f #{save_dir+package_file}`
 
-  tmp = open(save_dir+package_file.gsub(/\.gz$/, "")).read
-  tmp.each do |data|
-    tmp1 = data.split(/,/)
-    file = tmp1[0].to_s
-    md5sum = tmp1[1].gsub(/\n/, "").to_s
+tmp = open(save_dir+package_file.gsub(/\.gz$/, "")).read
+tmp.each do |data|
+   tmp1 = data.split(/,/)
+   file = tmp1[0].to_s
+   md5sum = tmp1[1].gsub(/\n/, "").to_s
 
-    if File.exist?(save_dir+file)
+   if File.exist?(save_dir+file)
       if md5sum != Digest::MD5.hexdigest(File.open(save_dir+file).read).to_s
-        File.delete(save_dir+file)
-        `wget -q -P #{save_dir} #{package_url+file}`
+         File.delete(save_dir+file)
+         `wget -q -P #{save_dir} #{package_url+file}`
       end
-    else
+   else
       `wget -q -P #{save_dir} #{package_url+file}`
-    end
-  end
+   end
+end
--- scripts/allways/cachetoprgdir.rb
+++ scripts/allways/cachetoprgdir.rb
@@ -2,16 +2,16 @@
 
 require "readline"
 
-  package_file = ARGV[0]  # ex. patch2.6.0.gz
-  save_dir = ARGV[1]+"/"  # ex. /var/lib/jma-receipt/cache/patch/
-  prg_dir = ARGV[2]+"/"   # ex. /home/orca/orca-prg2.6.0/
+package_file = ARGV[0]  # ex. patch2.6.0.gz
+save_dir = ARGV[1]+"/"  # ex. /var/lib/jma-receipt/patch/
+prg_dir = ARGV[2]+"/"   # ex. /home/orca/orca-prg2.6.0/
 
-  if File.exist?(save_dir+package_file)
-    `gunzip -f #{save_dir+package_file}`
-  end
-  if File.exist?(save_dir+package_file.gsub(/\.gz$/, ""))
-    tmp = open(save_dir+package_file.gsub(/\.gz$/, "")).read
-    tmp.each do |data|
+if File.exist?(save_dir+package_file)
+   `gunzip -f #{save_dir+package_file}`
+end
+if File.exist?(save_dir+package_file.gsub(/\.gz$/, ""))
+   tmp = open(save_dir+package_file.gsub(/\.gz$/, "")).read
+   tmp.each do |data|
       tmp1 = data.split(/,/)
       file = tmp1[0].to_s
 
@@ -20,5 +20,5 @@ require "readline"
         `mv #{prg_dir}/patch/* #{prg_dir}`
         `rmdir #{prg_dir}/patch`
       end
-    end
-  end
+   end
+end
