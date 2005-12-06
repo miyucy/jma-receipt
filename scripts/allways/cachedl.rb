dc52b9c5b72b27823787253ae234f0bda6e3570a
tosihiko <tosihiko@cvs.orca.med.or.jp>
2005-12-06 16:02:54 +0900

    オフラインメンテナンス組込み

--- scripts/allways/cachedl.rb
+++ scripts/allways/cachedl.rb
@@ -4,8 +4,10 @@ require "digest/md5"
 
 package_file = ARGV[0]  # ex. patch2.6.0.gz
 save_dir = ARGV[1]+"/"  # ex. /var/lib/jma-receipt/patch/
-package_url = "ftp://"+ARGV[2]+"/"
-  
+package_url = ARGV[2]+"/"
+offline = ARGV[3]	# ex. online or offline
+wgetoption = ARGV[4]	# ex. --passive-ftp
+
 if File.exist?(save_dir+package_file)
    File.delete(save_dir+package_file)
 end
@@ -13,8 +15,12 @@ if File.exist?(save_dir+package_file.gsub(/\.gz$/, ""))
    File.delete(save_dir+package_file.gsub(/\.gz$/, ""))
 end
 
+if offline == 'offline'
+   `cp #{package_url+package_file} #{save_dir}`
+else
 # ftp.orca.med.or.jp よりダウンロード
-`wget -q --passive-ftp -P #{save_dir} #{package_url+package_file}`
+   `wget -q #{wgetoption} -P #{save_dir} #{package_url+package_file}`
+end
 `gunzip -f #{save_dir+package_file}`
 
 tmp = open(save_dir+package_file.gsub(/\.gz$/, "")).read
@@ -26,9 +32,17 @@ tmp.each do |data|
    if File.exist?(save_dir+file)
       if md5sum != Digest::MD5.hexdigest(File.open(save_dir+file).read).to_s
          File.delete(save_dir+file)
-         `wget -q --passive-ftp -P #{save_dir} #{package_url+file}`
+	 if offline == 'offline'
+	    `cp #{package_url+file} #{save_dir}`
+	 else
+	    `wget -q #{wgetoption} -P #{save_dir} #{package_url+file}`
+	 end
       end
    else
-      `wget -q --passive-ftp -P #{save_dir} #{package_url+file}`
+      if offline == 'offline'
+         `cp #{package_url+file} #{save_dir}`
+      else
+         `wget -q #{wgetoption} -P #{save_dir} #{package_url+file}`
+      end
    end
 end
