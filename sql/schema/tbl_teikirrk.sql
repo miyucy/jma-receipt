7202ff8d8fafa0b82d294fda5e8d0a9762785283
ota <ota@cvs.orca.med.or.jp>
2006-01-23 14:06:02 +0900

    構造変更 （作成区分、請求開始日、請求終了日、請求点数（合計）、請求金額（合計）、 再請求区分を削除。期別を追加）

--- sql/schema/tbl_teikirrk.sql
+++ sql/schema/tbl_teikirrk.sql
@@ -1,17 +1,12 @@
 CREATE TABLE "tbl_teikirrk" (
 	"sryym" character(6) NOT NULL,
-	"sakkbn" character(1) NOT NULL,
-	"skystymd" character(8),
-	"skyedymd" character(8),
-	"skyten" numeric(9,0),
-	"skymoney" numeric(9,0),
-	"saiskykbn" character(1),
+	"kbt" character(1) NOT NULL,
 	"termid" character varying(16),
 	"opid" character varying(16),
 	"creymd" character varying(8),
 	"upymd" character varying(8),
 	"uphms" character varying(6),
-	Constraint "tbl_teikirrk_primary_key" Primary Key ("sryym", "sakkbn")
+	Constraint "tbl_teikirrk_primary_key" Primary Key ("sryym", "kbt")
 );
 
 COMMENT ON TABLE "tbl_teikirrk" IS 'Äê´üÀÁµáÍúÎò';
