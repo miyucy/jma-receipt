08daa23d9debcf0f8b27c4e82cbc7e671185aa6c
ota <ota@cvs.orca.med.or.jp>
2006-01-23 14:06:56 +0900

    構造変更（削除フラグ、処理区分、期別を追加）

--- sql/schema/tbl_ptteikirrk.sql
+++ sql/schema/tbl_ptteikirrk.sql
@@ -4,13 +4,16 @@ CREATE TABLE "tbl_ptteikirrk" (
 	"sryym" character(6) NOT NULL,
 	"sakkbn" character(1) NOT NULL,
 	"hkncombinum" numeric(4,0) NOT NULL,
+	"denpnum" numeric(7,0) NOT NULL,
+	"delflg" numeric(1,0),
+	"syokbn" character(1),
+	"kbt" character(1),
 	"skystymd" character(8),
 	"skyedymd" character(8),
 	"skyten" numeric(9,0),
 	"skymoney_tax_sai" numeric(7,0),
 	"skymoney" numeric(9,0),
 	"rrknum" numeric(3,0),
-	"denpnum" numeric(7,0),
 	"denpedanum" numeric(2,0),
 	"skyupdflg" numeric(1,0),
 	"termid" character varying(16),
@@ -18,7 +21,7 @@ CREATE TABLE "tbl_ptteikirrk" (
 	"creymd" character varying(8),
 	"upymd" character varying(8),
 	"uphms" character varying(6),
-	Constraint "tbl_ptteikirrk_primary_key" Primary Key ("hospid", "ptid", "sryym", "sakkbn", "hkncombinum")
+	Constraint "tbl_ptteikirrk_primary_key" Primary Key ("hospid", "ptid", "sryym", "sakkbn", "hkncombinum", "denpnum")
 );
 
 COMMENT ON TABLE "tbl_ptteikirrk" IS '´µ¼ÔÄê´üÀÁµáÍúÎò';
