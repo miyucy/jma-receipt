4496c37687dde684ef977e867cc19ca4a6e0f974
azu <azu@cvs.orca.med.or.jp>
2006-03-15 09:48:44 +0900

    システム予約コード追加

--- sql/orcadbup_tensu_2.8.0.sql
+++ sql/orcadbup_tensu_2.8.0.sql
@@ -8,6 +8,9 @@
 -- 099999918 食事（朝）              --
 -- 099999919 食事（昼）              --
 -- 099999920 食事（夕）              --
+-- システム管理病棟設定表示用コード  --
+-- 099999950 有床診療所入院基本料１  --
+-- 099999951 有床診療所入院基本料２  --
 
 insert into tbl_tensu (srycd,yukostymd,yukoedymd,srykbn,srysyukbn,yukoketa,name,kanayukoketa,kananame,tensikibetu,ten,tanicd,datakbn,
 chuksncd,chuksntsuban,kgnage,jgnage,ageksnkgn1,ageksnjgn1,ageksnsrycd1,ageksnkgn2,ageksnjgn2,ageksnsrycd2,ageksnkgn3,
@@ -31,5 +34,22 @@ ageksnjgn3,ageksnsrycd3,ageksnkgn4,ageksnjgn4,ageksnsrycd4,cdkbn_kbn,kokuchi_kbn
 chgymd,haisiymd,keikasochiymd,creymd,upymd,uphms)
 values
 ('099999920','20060401','99999999','99','990',5,'食事（夕）',8,'ショクジ（ユウ）',0,0,'000',1,
+'0000','0','00','00','00','00','         ','00','00','         ','00','00','         ','00','00','         ',' ',' ','         ',
+'            ','         ','        ','        ','        ','20060401','        ','000000');
+
+insert into tbl_tensu (srycd,yukostymd,yukoedymd,srykbn,srysyukbn,yukoketa,name,kanayukoketa,kananame,tensikibetu,ten,tanicd,datakbn,
+chuksncd,chuksntsuban,kgnage,jgnage,ageksnkgn1,ageksnjgn1,ageksnsrycd1,ageksnkgn2,ageksnjgn2,ageksnsrycd2,ageksnkgn3,
+ageksnjgn3,ageksnsrycd3,ageksnkgn4,ageksnjgn4,ageksnsrycd4,cdkbn_kbn,kokuchi_kbn,idokanren,yakkakjncd,syomeikanren,
+chgymd,haisiymd,keikasochiymd,creymd,upymd,uphms)
+values
+('099999950','20060401','99999999','99','990',11,'有床診療所入院基本料１',6,'ユウショウ１',0,0,'000',1,
+'0000','0','00','00','00','00','         ','00','00','         ','00','00','         ','00','00','         ',' ',' ','         ',
+'            ','         ','        ','        ','        ','20060401','        ','000000');
+insert into tbl_tensu (srycd,yukostymd,yukoedymd,srykbn,srysyukbn,yukoketa,name,kanayukoketa,kananame,tensikibetu,ten,tanicd,datakbn,
+chuksncd,chuksntsuban,kgnage,jgnage,ageksnkgn1,ageksnjgn1,ageksnsrycd1,ageksnkgn2,ageksnjgn2,ageksnsrycd2,ageksnkgn3,
+ageksnjgn3,ageksnsrycd3,ageksnkgn4,ageksnjgn4,ageksnsrycd4,cdkbn_kbn,kokuchi_kbn,idokanren,yakkakjncd,syomeikanren,
+chgymd,haisiymd,keikasochiymd,creymd,upymd,uphms)
+values
+('099999951','20060401','99999999','99','990',11,'有床診療所入院基本料２',6,'ヨウウショウ２',0,0,'000',1,
 '0000','0','00','00','00','00','         ','00','00','         ','00','00','         ','00','00','         ',' ',' ','         ',
 '            ','         ','        ','        ','        ','20060401','        ','000000');
