fee8eaff66f69d0aea7532e708fcc84c9a59843a
azu <azu@cvs.orca.med.or.jp>
2006-07-06 16:48:48 +0900

    領収書対応

---
+++ sql/3.1/orcadbdr_view_3.1.0-1.sql
@@ -0,0 +1,9 @@
+--                                    --
+--  収納テーブル変更に伴う処理        --
+--                                    --
+
+\set ON_ERROR_STOP
+
+DROP VIEW view_bd001 ;
+DROP VIEW view_bd002 ;
+DROP VIEW view_bd003 ;
