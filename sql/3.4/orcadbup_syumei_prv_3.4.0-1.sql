e5393ca4bc5609985e0951155bb0a696f656265f
ota <ota@cvs.orca.med.or.jp>
2007-01-19 17:42:55 +0900

    収納明細構造変更、収納履歴テーブル作成

--- sql/3.4/orcadbup_syumei_prv_3.4.0-1.sql
+++ sql/3.4/orcadbup_syumei_prv_3.4.0-1.sql
@@ -1,90 +1,24 @@
 \set ON_ERROR_STOP
 --
--- ÊÝ¸±ÅÀ¿ô(HKNTEN)ÄÉ²Ã
--- ÊÝ¸±ÀÁµá³Û(HKNMONEY)ÄÉ²Ã
--- ¼«ÈñÀÁµá³Û(JIHIMONEY)ÄÉ²Ã
--- ¿©»öÀÁµá³Û(SKJMONEY)ÄÉ²Ã
--- À¸³èÀÁµá³Û(LIFEMONEY)ÄÉ²Ã
--- Ä´À°¶â(CHOSEI)ÄÉ²Ã
--- ¸ºÌÈ¶â³Û(DISCOUNTMONEY)ÄÉ²Ã
--- ¼«ÈñÆâÌõ(JIHI_1¡ÁJIHI_10)ÄÉ²Ã
--- ¼¼ÎÁº¹³Û(RMSAGAKU)ÄÉ²Ã
+--
+-- ¼ýÇ¼ÍúÎòÈÖ¹æ(SYURRKNUM)ÄÉ²Ã
+-- ¼ýÇ¼ÍúÎò»ÞÈÖ¹æ(SYURRKEDANUM)ÄÉ²Ã
+-- ¼ýÇ¼ÍúÎò¹¹¿·¶èÊ¬(SYURRKUPDKBN)ÄÉ²Ã
 -- 
 -- Create Date : 2006/12/19           --
 --
 --  ¹àÌÜÄÉ²Ã                        --
 alter table TBL_SYUMEI_PRV
-   add column HKNTEN		numeric(08);
+   add column SYURRKNUM		integer;
 alter table TBL_SYUMEI_PRV
-   add column HKNMONEY		numeric(08);
+   add column SYUEDANUM		integer;
 alter table TBL_SYUMEI_PRV
-   add column JIHIMONEY		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column SKJMONEY		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column LIFEMONEY		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column CHOSEI		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column DISCOUNTMONEY	numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_1		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_2		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_3		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_4		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_5		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_6		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_7		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_8		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_9		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column JIHI_10		numeric(08);
-alter table TBL_SYUMEI_PRV
-   add column RMSAGAKU		numeric(08);
+   add column SYURRKUPDKBN	char(1);
 
 
 --  ¥¼¥íÀßÄê                       --
 alter table TBL_SYUMEI_PRV
-   alter HKNTEN		set default 0;
+   alter SYURRKNUM	set default 0;
 alter table TBL_SYUMEI_PRV
-   alter HKNMONEY	set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHIMONEY	set default 0;
-alter table TBL_SYUMEI_PRV
-   alter SKJMONEY	set default 0;
-alter table TBL_SYUMEI_PRV
-   alter LIFEMONEY	set default 0;
-alter table TBL_SYUMEI_PRV
-   alter CHOSEI		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter DISCOUNTMONEY	set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_1		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_2		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_3		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_4		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_5		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_6		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_7		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_8		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_9		set default 0;
-alter table TBL_SYUMEI_PRV
-   alter JIHI_10	set default 0;
-alter table TBL_SYUMEI_PRV
-   alter RMSAGAKU	set default 0;
+   alter SYUEDANUM	set default 0;
+
