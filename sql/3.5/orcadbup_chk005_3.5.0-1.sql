05b04c8d8299d3c632774b70f3da7688be14eb95
tosihiko <tosihiko@cvs.orca.med.or.jp>
2007-05-02 15:32:04 +0900

    グループ診療：テーブル整理

---
+++ sql/3.5/orcadbup_chk005_3.5.0-1.sql
@@ -0,0 +1,6 @@
+\set ON_ERROR_STOP
+
+alter table tbl_chk005 add  column hospnum  integer;
+update tbl_chk005 set hospnum = 1 ;
+alter table tbl_chk005 drop constraint  tbl_chk005_primary_key;
+alter table tbl_chk005 add constraint tbl_chk005_primary_key primary key (hospnum, chkkbn, id, rennum);
