d138829f0dfafc4568932ee8454acf81cbbd8ee2
tosihiko <tosihiko@cvs.orca.med.or.jp>
2007-05-02 18:54:35 +0900

    グループ診療：テーブル整理

--- sql/3.5/orcadbup_syumei_3.5.0-1.sql
+++ sql/3.5/orcadbup_syumei_3.5.0-1.sql
@@ -1,27 +1,6 @@
 \set ON_ERROR_STOP
---
--- ¼ıÇ¼ÍúÎòÈÖ¹æ(SYURRKNUM)ÄÉ²Ã
--- ¼ıÇ¼ÍúÎò»ŞÈÖ¹æ(SYURRKEDANUM)ÄÉ²Ã
--- ¼ıÇ¼ÍúÎò¹¹¿·¶èÊ¬(SYURRKUPDKBN)ÄÉ²Ã
--- 
--- Create Date : 2006/12/19           --
---
---  ¹àÌÜÄÉ²Ã                        --
-alter table TBL_SYUMEI
-   add column SYURRKNUM		integer;
-alter table TBL_SYUMEI
-   add column SYUEDANUM		integer;
-alter table TBL_SYUMEI
-   add column SYURRKUPDKBN	char(1);
 
-
---  ¥¼¥íÀßÄê                       --
-alter table TBL_SYUMEI
-   alter SYURRKNUM	set default 0;
-alter table TBL_SYUMEI
-   alter SYUEDANUM	set default 0;
-
-alter table TBL_SYUMEI add  column hospnum  integer;
-alter table TBL_SYUMEI drop column hospid;
-update TBL_SYUMEI set hospnum = 1 ;
-alter table TBL_SYUMEI add constraint tbl_syumei_primary_key primary key (hospnum,nyugaikbn,ptid,denpnum,denpedanum);
+alter table tbl_syumei add  column hospnum  integer;
+alter table tbl_syumei drop column hospid;
+update tbl_syumei set hospnum = 1 ;
+alter table tbl_syumei add constraint tbl_syumei_primary_key primary key (hospnum, nyugaikbn, ptid, denpnum, denpedanum);
--- sql/3.5/orcadbup_syumei_prv_3.5.0-1.sql
+++ sql/3.5/orcadbup_syumei_prv_3.5.0-1.sql
@@ -1,28 +1,6 @@
 \set ON_ERROR_STOP
---
---
--- ¼ıÇ¼ÍúÎòÈÖ¹æ(SYURRKNUM)ÄÉ²Ã
--- ¼ıÇ¼ÍúÎò»ŞÈÖ¹æ(SYURRKEDANUM)ÄÉ²Ã
--- ¼ıÇ¼ÍúÎò¹¹¿·¶èÊ¬(SYURRKUPDKBN)ÄÉ²Ã
--- 
--- Create Date : 2006/12/19           --
---
---  ¹àÌÜÄÉ²Ã                        --
-alter table TBL_SYUMEI_PRV
-   add column SYURRKNUM		integer;
-alter table TBL_SYUMEI_PRV
-   add column SYUEDANUM		integer;
-alter table TBL_SYUMEI_PRV
-   add column SYURRKUPDKBN	char(1);
 
-
---  ¥¼¥íÀßÄê                       --
-alter table TBL_SYUMEI_PRV
-   alter SYURRKNUM	set default 0;
-alter table TBL_SYUMEI_PRV
-   alter SYUEDANUM	set default 0;
-
-alter table TBL_SYUMEI_PRV add  column hospnum  integer;
-alter table TBL_SYUMEI_PRV drop column hospid;
-update TBL_SYUMEI_PRV set hospnum = 1 ;
-alter table TBL_SYUMEI_PRV add constraint tbl_syumei_primary_prv_key primary key (hospnum,nyugaikbn,ptid,denpnum,denpedanum,creymd);
+alter table tbl_syumei_prv add  column hospnum  integer;
+alter table tbl_syumei_prv drop column hospid;
+update tbl_syumei_prv set hospnum = 1 ;
+alter table tbl_syumei_prv add constraint tbl_syumei_prv_primary_key primary key (hospnum, nyugaikbn, ptid, denpnum, denpedanum, creymd);
