d2e93a1f0deb11f617e1956f72597353e10ff957
tosihiko <tosihiko@cvs.orca.med.or.jp>
2007-07-05 15:07:35 +0900

    グループ診療対応：ＤＢ管理

--- sql/3.5/orcadbup_dbkanri_3.5.0-1.sql
+++ sql/3.5/orcadbup_dbkanri_3.5.0-1.sql
@@ -3,8 +3,7 @@
 -- 管理コードORCADB00以外は削除
 delete from tbl_dbkanri where kanricd <> 'ORCADB00';
 --
-alter table tbl_dbkanri drop constraint  tbl_dbkanri_primary_key;
-alter table tbl_dbkanri drop column kanricd;
+--alter table tbl_dbkanri drop constraint  tbl_dbkanri_primary_key;
 alter table tbl_dbkanri drop column dbrversion1;
 alter table tbl_dbkanri drop column dbrversion2;
 --
@@ -13,10 +12,6 @@ update tbl_dbkanri set version = trim(version);
 update tbl_dbkanri set dbsversion1 = trim(dbsversion1);
 update tbl_dbkanri set dbsversion2 = trim(dbsversion2);
 
+update tbl_dbkanri set upymd = to_char(now(),'yyyymmdd'), uphms = to_char(now(),'hh24miss');
 --
 vacuum full tbl_dbkanri;
-
---alter table tbl_dbkanri add  column hospnum  integer;
---update tbl_dbkanri set hospnum = 1 ;
---alter table tbl_dbkanri drop constraint  tbl_dbkanri_primary_key;
---alter table tbl_dbkanri add constraint tbl_dbkanri_primary_key primary key (hospnum,kanricd);
