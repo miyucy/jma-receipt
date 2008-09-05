44584cbf3dee4096f7fa0b52f5ae4c9beb66c3c9
ota <ota@cvs.orca.med.or.jp>
2008-09-05 11:16:17 +0900

    カラム名変更

--- sql/4.3/orcadbup_cron_4.3.0-1.sql
+++ sql/4.3/orcadbup_cron_4.3.0-1.sql
@@ -9,7 +9,7 @@
 
 DELETE from tbl_cron where cronid in (1,2);
 
-COPY tbl_cron (cronid, run, name, min, "hour", dom, mon, dow, script, termid, opid, creymd, upymd, uphms) FROM stdin;
+COPY tbl_cron (cronid, run, name, crnmin, crnhour, crndom, crnmon, crndow, script, termid, opid, creymd, upymd, uphms) FROM stdin;
 1	0	ＤＢバックアップ	00	01	* 	* 	*	db-backup.sh			20080828	20080828	032359
 2	0	マスタ更新	00	01 	* 	* 	*	run_master_upgrade.sh			20080901	20080901	145930
 \.
