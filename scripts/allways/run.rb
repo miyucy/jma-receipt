d482b8d6ae4c739e0fb3b820a0ff96bcdb7f0ec5
ikegam <ikegam@cvs.orca.med.or.jp>
2008-10-03 14:32:44 +0900

    fix syntax error

--- scripts/allways/run.rb
+++ scripts/allways/run.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 require 'date'
 
-list = `psql orca -At -F ',' -c 'SELECT CRNDOW,CRNMON,CRNDOM,CRNHOUR,SCRIPT from tbl_cron where RUN = \'1\'\;'`.split("\n").map{|r|
+list = `psql orca -At -F ',' -c "SELECT CRNDOW,CRNMON,CRNDOM,CRNHOUR,SCRIPT from tbl_cron where RUN = \'1\'\;"`.split("\n").map{|r|
   r = r.split(/,/).map{|c| c = "." * c.length if c =~ /^\*/; c }
   [sprintf("%s %s/%s/.. %s", r[0], r[1], r[2], r[3]), r[4..-1].join(" ")]
 }
@@ -9,6 +9,6 @@ time = DateTime.now.strftime("%w %D %H")
 
 list.each { |cmd|
   next unless /#{cmd[0]}/ =~ time
-  open("/tmp/jma-receipt-cron-lock", "w"){|f| f.flock(File::LOCK_EX); puts `#{cmd[1]}`; f.flock(File::LOCK_UN) }
+  open("/tmp/jma-receipt-cron-lock", "w"){|f| f.flock(File::LOCK_EX); `#{cmd[1]}`; f.flock(File::LOCK_UN) }
 }
 
