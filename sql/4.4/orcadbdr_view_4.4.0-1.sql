ad574204b95c9641d15c41e27ad43fc065411358
ota <ota@cvs.orca.med.or.jp>
2009-02-17 13:19:51 +0900

    収納ビュー再生成

--- sql/4.4/orcadbdr_view_4.4.0-1.sql
+++ sql/4.4/orcadbdr_view_4.4.0-1.sql
@@ -2,6 +2,9 @@
 -- ¥Ó¥å¡¼ºï½ü                                   --
 -- view_q001   ¾È²ñ£²                           --
 -- view_q002   ¾È²ñ£³                           --
+-- view_bd001  ¼ýÇ¼£±                           --
+-- view_bd002  ¼ýÇ¼£²                           --
+-- view_bd003  ¼ýÇ¼£³                           --
 --                                              --
 -- Create Date : 2008/06/30                     --
 --                                              --
@@ -9,3 +12,6 @@
 
 drop view view_q002;
 drop view view_q003;
+drop view view_bd001;
+drop view view_bd002;
+drop view view_bd003;
