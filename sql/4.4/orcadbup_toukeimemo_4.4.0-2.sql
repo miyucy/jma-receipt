81be0f579f73b23ebe0e949b93ac42f70765b20b
azu <azu@cvs.orca.med.or.jp>
2009-02-25 17:55:05 +0900

    未コード化病名リスト（統計メモ修正）

--- sql/4.4/orcadbup_toukeimemo_4.4.0-2.sql
+++ sql/4.4/orcadbup_toukeimemo_4.4.0-2.sql
@@ -11,7 +11,7 @@ delete from tbl_toukeimemo where pgid = 'ORCBG021' and kanricd = '0000' and kbnc
 delete from tbl_toukeimemo where hospnum = 0;
 
 COPY tbl_toukeimemo FROM stdin;
-ORCBG021	0000	000     	00000000	99999999									診療年月　　診療年月を入力します。\n印刷区分　　０：全ての病名を対象とします。\n　　　　　　１：病名コードまたは自院コードの入力がある\n　　　　　　　　病名のみ対象とします。\n処理区分　　０：入力された診療年月に診療がある患者のみ\n　　　　　　　　対象とします。\n　　　　　　１：全ての患者を対象とします。\n\n※入力された診療年月より前に転帰となった病名は処理対象外です。\n\n\n			20090224	20090224	150000	0	 		
+ORCBG021	0000	000     	00000000	99999999									診療年月　　診療年月を入力します。\n印刷区分　　０：全ての病名を対象とします。\n　　　　　　１：自院コードまたは病名コードで入力された\n　　　　　　　　病名を対象とします。\n処理区分　　０：入力された診療年月に診療がある患者のみ\n　　　　　　　　対象とします。\n　　　　　　１：全ての患者を対象とします。\n\n※入力された診療年月より前に転帰となった病名は処理対象外です。\n\n\n			20090224	20090224	150000	0	 		
 \.
 
 insert into tbl_toukeimemo select a.pgid,a.kanricd,a.kbncd,a.styukymd,a.edyukymd,a.para1,a.para2,a.para3,a.para4,a.para5,
