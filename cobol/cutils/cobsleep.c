a7c8a463bca9360e4194c16c6890c90daa00b4fd
saitou <saitou@cvs.orca.med.or.jp>
2009-03-10 16:35:12 +0900

    add COBOL sleep (for debug)

---
+++ cobol/cutils/cobsleep.c
@@ -0,0 +1,17 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+void
+cobsleep (char *interval)
+{
+  int s;
+  char *tmp;
+
+  tmp = strndup(interval,2);
+  s = atoi(tmp);
+  printf("%d\n", s);
+  sleep(s);
+  free(tmp);
+}
