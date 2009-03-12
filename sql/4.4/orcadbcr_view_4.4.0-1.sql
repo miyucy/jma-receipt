d457d097bb4fd811eb9b73bc8abd8b6e6a80a28b
ota <ota@cvs.orca.med.or.jp>
2009-03-12 14:33:14 +0900

    照会ビュー１修正(977の考慮追加)

--- sql/4.4/orcadbcr_view_4.4.0-1.sql
+++ sql/4.4/orcadbcr_view_4.4.0-1.sql
@@ -5,6 +5,7 @@
 -- view_bd001  ¼ýÇ¼£±                           --
 -- view_bd002  ¼ýÇ¼£²                           --
 -- view_bd003  ¼ýÇ¼£³                           --
+-- view_q001   ¾È²ñ£±                           --
 --                                              --
 -- Create Date : 2009/01/15                     --
 --                                              --
@@ -63,7 +64,7 @@ COMMENT
 
 
 CREATE VIEW view_bd001 AS
-    SELECT a.hospnum, a.ptid, b.ptnum, c.name, c.sex, c.birthday, a.sryka, a.denpprtymd, a.skymoney, a.nyukin_total FROM tbl_syunou_main a, tbl_ptnum b, tbl_ptinf c WHERE ((((((((a.hospnum = b.hospnum) AND (a.ptid = b.ptid)) AND (a.hospnum = c.hospnum)) AND (a.ptid = c.ptid)) AND (a.denpjtikbn <> '3'::bpchar)) AND (a.denpjtikbn <> '7'::bpchar)) AND (a.createkbn <> '3'::bpchar)) AND (c.tstptnumkbn <> '1'::bpchar));
+    SELECT a.hospnum, a.ptid, b.ptnum, c.name, c.sex, c.birthday, a.sryka, a.denpprtymd, a.skymoney, a.nyukin_total FROM tbl_syunou_main a, tbl_ptnum b, tbl_ptinf c WHERE ((((((((a.hospnum = b.hospnum) AND (a.ptid = b.ptid)) AND (a.hospnum = c.hospnum)) AND (a.ptid = c.ptid)) AND (a.denpjtikbn <> '3')) AND (a.denpjtikbn <> '7')) AND (a.createkbn <> '3')) AND (c.tstptnumkbn <> '1'));
 
 COMMENT ON VIEW view_bd001 IS '¼ýÇ¼¥Ó¥å¡¼';
 
@@ -394,4 +395,89 @@ CREATE VIEW view_bd003 AS
 
 COMMENT ON VIEW view_bd003 IS '¼ýÇ¼¥Ó¥å¡¼3';
 
+
+CREATE VIEW view_q001 AS
+    SELECT tbl_hkncombi.hospnum,
+           tbl_hkncombi.ptid,
+           tbl_hkncombi.hkncombinum,
+           view_q004.nyugaikbn,
+           view_q004.sryka,
+           view_q004.sryymd,
+           tbl_pthkninf.hknid,
+           tbl_pthkninf.hknnum,
+           tbl_pthkninf.hknjanum,
+           tbl_pthkninf.honkzkkbn,
+           tbl_pthkninf.kigo,
+           tbl_pthkninf.num,
+           tbl_ptkohinf.kohid,
+           tbl_ptkohinf.kohnum,
+           tbl_ptkohinf.paykbn,
+           tbl_ptkohinf.ftnjanum 
+      FROM (((tbl_hkncombi JOIN view_q004 
+        ON ((((tbl_hkncombi.hospnum = view_q004.hospnum) 
+       AND (tbl_hkncombi.ptid = view_q004.ptid)) 
+       AND (tbl_hkncombi.hkncombinum = to_number((view_q004.hkncombinum),
+           '9999'))))) 
+      LEFT JOIN tbl_pthkninf 
+        ON (((((tbl_hkncombi.hospnum = tbl_pthkninf.hospnum) 
+       AND (tbl_hkncombi.ptid = tbl_pthkninf.ptid)) 
+       AND (tbl_hkncombi.hknid = tbl_pthkninf.hknid)) 
+       AND (tbl_hkncombi.hknnum = tbl_pthkninf.hknnum)))) 
+      LEFT JOIN tbl_ptkohinf 
+        ON ((((tbl_hkncombi.hospnum = tbl_ptkohinf.hospnum) 
+       AND (tbl_hkncombi.ptid = tbl_ptkohinf.ptid)) 
+       AND (((((tbl_hkncombi.koh1id = tbl_ptkohinf.kohid) 
+       AND (tbl_hkncombi.koh1hknnum = tbl_ptkohinf.kohnum)) 
+        OR ((tbl_hkncombi.koh2id = tbl_ptkohinf.kohid) 
+       AND (tbl_hkncombi.koh2hknnum = tbl_ptkohinf.kohnum))) 
+        OR ((tbl_hkncombi.koh3id = tbl_ptkohinf.kohid) 
+       AND (tbl_hkncombi.koh3hknnum = tbl_ptkohinf.kohnum))) 
+        OR ((tbl_hkncombi.koh4id = tbl_ptkohinf.kohid) 
+       AND (tbl_hkncombi.koh4hknnum = tbl_ptkohinf.kohnum)))))) UNION SELECT tbl_hkncombi.hospnum,
+           tbl_hkncombi.ptid,
+           tbl_hkncombi.hkncombinum,
+           view_q004.nyugaikbn,
+           view_q004.sryka,
+           view_q004.sryymd,
+           tbl_pthkninf.hknid,
+           tbl_pthkninf.hknnum,
+           tbl_pthkninf.hknjanum,
+           tbl_pthkninf.honkzkkbn,
+           tbl_pthkninf.kigo,
+           tbl_pthkninf.num,
+           tbl_ptkohinf.kohid,
+           tbl_ptkohinf.kohnum,
+           tbl_ptkohinf.paykbn,
+           tbl_ptkohinf.ftnjanum 
+      FROM ((view_q004 JOIN tbl_hkncombi 
+        ON ((((view_q004.hospnum = tbl_hkncombi.hospnum) 
+       AND (view_q004.ptid = tbl_hkncombi.ptid)) 
+       AND (to_number((view_q004.hkncombinum),
+           '9999') = tbl_hkncombi.hkncombinum)))) 
+      LEFT JOIN tbl_pthkninf 
+        ON (((((tbl_hkncombi.hospnum = tbl_pthkninf.hospnum) 
+       AND (tbl_hkncombi.ptid = tbl_pthkninf.ptid)) 
+       AND (tbl_hkncombi.hknid = tbl_pthkninf.hknid)) 
+       AND (tbl_hkncombi.hknnum = tbl_pthkninf.hknnum)))),
+           tbl_ptkohinf 
+     WHERE (((((view_q004.hospnum = tbl_ptkohinf.hospnum) 
+       AND (view_q004.ptid = tbl_ptkohinf.ptid)) 
+       AND (((((((((((((tbl_ptkohinf.kohnum = '960') 
+        OR (tbl_ptkohinf.kohnum = '961')) 
+        OR (tbl_ptkohinf.kohnum = '962')) 
+        OR (tbl_ptkohinf.kohnum = '963')) 
+        OR (tbl_ptkohinf.kohnum = '964')) 
+        OR (tbl_ptkohinf.kohnum = '965')) 
+        OR (tbl_ptkohinf.kohnum = '966')) 
+        OR (tbl_ptkohinf.kohnum = '967')) 
+        OR (tbl_ptkohinf.kohnum = '968')) 
+        OR (tbl_ptkohinf.kohnum = '969')) 
+        OR (tbl_ptkohinf.kohnum = '977')) 
+        OR (tbl_ptkohinf.kohnum = '978')) 
+        OR (tbl_ptkohinf.kohnum = '979'))) 
+       AND (view_q004.sryymd >= tbl_ptkohinf.tekstymd)) 
+       AND (view_q004.sryymd <= tbl_ptkohinf.tekedymd));
+
+COMMENT 
+        ON VIEW view_q001 IS '´µ¼Ô¾È²ñ¥Ó¥å¡¼1';
 
