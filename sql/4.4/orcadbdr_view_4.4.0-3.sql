5dbba1466d307e3b369c045efd45e3eea24196ad
ota <ota@cvs.orca.med.or.jp>
2009-06-26 15:02:37 +0900

    view_q002 構造変更対応

--- sql/4.4/orcadbdr_view_4.4.0-3.sql
+++ sql/4.4/orcadbdr_view_4.4.0-3.sql
@@ -1,9 +1,9 @@
 --                                              --
 -- ビュー削除                                   --
--- view_q001   照会１                           --
+-- view_q002   照会２                           --
 --                                              --
--- Create Date : 2009/04/14                     --
+-- Create Date : 2009/06/26                     --
 --                                              --
 \set ON_ERROR_STOP
 
-drop view view_q001;
+drop view view_q002;
