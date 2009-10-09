4496c35779d1c060175ed933ff04296f3646455e
fujihara <fujihara@cvs.orca.med.or.jp>
2009-10-09 16:25:37 +0900

    レイアウト変更

--- sql/4.5/orcadbcr_toukeicsv_b_4.5.0-1.sql
+++ sql/4.5/orcadbcr_toukeicsv_b_4.5.0-1.sql
@@ -1,23 +1,30 @@
+\set ON_ERROR_STOP
+--
+-- Åý·×£Ã£Ó£ÖÌÀºÙ¥Æ¡¼¥Ö¥ë
+--
+-- Create Date : 2009/09/27         --
+
 create	table	tbl_toukeicsv_b	(
-	HOSPNUM	numeric(2,0),
-	TBL_KEY	char(8),
-	RENNUM	numeric(4,0),
-	TBL_GROUP	char(14),
-	SHORI_RENNUM	numeric(4,0),
-	SHELLID	varchar(20),
-	SRYYM	varchar(6),
-	SRYYMD	varchar(8),
-	PTID	numeric(10,0),
-	CNT	numeric(5,0),
-	PUTFLG	char(1),
-	CSVDATA	varchar(20000),
-	CREYMD	varchar(8),
+	HOSPNUM		smallint	 DEFAULT 0 NOT NULL,
+	TBL_KEY		char(8)		 NOT NULL,
+	RENNUM		smallint	 DEFAULT 0 NOT NULL,
+	TBL_GROUP	char(14)	 NOT NULL,
+	SHORI_RENNUM	smallint	 DEFAULT 0 NOT NULL,
+	SHELLID		varchar(20)	 NOT NULL,
+	SRYYM		varchar(6)	 NOT NULL,
+	SRYYMD		varchar(8)	 NOT NULL,
+	PTID		bigint		 DEFAULT 0,
+	SEQ_NO		integer		 DEFAULT 0,
+	CSVDATA		varchar(20000)	 NOT NULL,
+	CREYMD		char(8),
+	UPYMD		char(8),
+	UPHMS		char(6),
 	primary	key(
 		HOSPNUM,
 		TBL_KEY,
 		RENNUM,
 		TBL_GROUP,
 		SHORI_RENNUM,
-		CNT
+		SEQ_NO
 	)
 );
