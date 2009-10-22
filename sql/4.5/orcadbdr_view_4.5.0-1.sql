461ba6aca596cf96d13d539413dfbd62aa1a47b9
ota <ota@cvs.orca.med.or.jp>
2009-10-22 17:39:02 +0900

    4.5.0-1に集約

--- sql/4.5/orcadbdr_view_4.5.0-1.sql
+++ sql/4.5/orcadbdr_view_4.5.0-1.sql
@@ -1,9 +1,15 @@
 --                                              --
 -- ビュー削除                                   --
 -- view_q001   照会１                           --
+-- view_q003   照会３                           --
+-- view_q002   照会２                           --
+-- view_i001   入院患者照会                     --
 --                                              --
 -- Create Date : 2009/04/14                     --
 --                                              --
 \set ON_ERROR_STOP
 
 drop view view_q001;
+drop view view_q003;
+drop view view_q002;
+drop view view_i001;
--- sql/4.5/orcadbdr_view_4.5.0-2.sql
+++
@@ -1,5 +0,0 @@
-\set ON_ERROR_STOP
-
-drop view view_q003;
-drop view view_q002;
-drop view view_i001;
