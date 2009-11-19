065a743df9fa6d0ba601bc5e21ee302868641aca
fujihara <fujihara@cvs.orca.med.or.jp>
2009-11-19 09:50:36 +0900

    varchar に変更

--- sql/4.5/orcadbup_seikyu_4.5.0-1.sql
+++ sql/4.5/orcadbup_seikyu_4.5.0-1.sql
@@ -1,10 +1,10 @@
 \set ON_ERROR_STOP
 
-alter table tbl_seikyu_main add column tokkinm1 char(12);
-alter table tbl_seikyu_main add column tokkinm2 char(12);
-alter table tbl_seikyu_main add column tokkinm3 char(12);
-alter table tbl_seikyu_main add column tokkinm4 char(12);
-alter table tbl_seikyu_main add column tokkinm5 char(12);
+alter table tbl_seikyu_main add column tokkinm1 varchar(12);
+alter table tbl_seikyu_main add column tokkinm2 varchar(12);
+alter table tbl_seikyu_main add column tokkinm3 varchar(12);
+alter table tbl_seikyu_main add column tokkinm4 varchar(12);
+alter table tbl_seikyu_main add column tokkinm5 varchar(12);
 update tbl_seikyu_main set tokkinm1 = ' ';
 update tbl_seikyu_main set tokkinm2 = ' ';
 update tbl_seikyu_main set tokkinm3 = ' ';
