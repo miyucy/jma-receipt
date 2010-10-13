b39aa90d438aea495c4abdd33c5d39a12fca5c92
tatano <tatano@cvs.orca.med.or.jp>
2010-10-13 13:36:29 +0900

    型変更

--- sql/4.6/orcadbup_tensuplus_4.6.0-1.sql
+++ sql/4.6/orcadbup_tensuplus_4.6.0-1.sql
@@ -9,6 +9,6 @@
 
 \set ON_ERROR_STOP
 
-alter table tbl_tensuplus add column jgncnt1w numeric(3,0);
+alter table tbl_tensuplus add column jgncnt1w smallint;
 alter table tbl_tensuplus alter jgncnt1w set default 0;
 update tbl_tensuplus set jgncnt1w = 0;
