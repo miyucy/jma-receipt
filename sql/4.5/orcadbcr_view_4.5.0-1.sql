de3b99d7709916d50d67bd30aa602fb743dae5b5
ota <ota@cvs.orca.med.or.jp>
2010-12-17 13:23:37 +0900

    view_q002更新

--- sql/4.5/orcadbcr_view_4.5.0-1.sql
+++ sql/4.5/orcadbcr_view_4.5.0-1.sql
@@ -118,61 +118,6 @@ CREATE VIEW view_q003 AS
 COMMENT 
         ON VIEW view_q003 IS '患者照会ビュー3';
 
-CREATE VIEW view_q002 AS
-    SELECT a.hospnum,
-           a.ptid,
-           a.nyugaikbn,
-           a.sryka,
-           a.srykbn,
-           a.srysyukbn,
-           b.sryymd,
-           a.srycd1,
-           a.srycd2,
-           a.srycd3,
-           a.srycd4,
-           a.srycd5 
-      FROM tbl_sryact a,
-           tbl_jyurrk b 
-     WHERE (((((a.hospnum = b.hospnum) 
-       AND (a.ptid = b.ptid)) 
-       AND (a.nyugaikbn = b.nyugaikbn)) 
-       AND (a.sryka = b.sryka)) 
-       AND (((((((((((((((a.zainum = b.zainum1) 
-        OR (a.zainum = b.zainum2)) 
-        OR (a.zainum = b.zainum3)) 
-        OR (a.zainum = b.zainum4)) 
-        OR (a.zainum = b.zainum5)) 
-        OR (a.zainum = b.zainum6)) 
-        OR (a.zainum = b.zainum7)) 
-        OR (a.zainum = b.zainum8)) 
-        OR (a.zainum = b.zainum9)) 
-        OR (a.zainum = b.zainum10)) 
-        OR (a.zainum = b.zainum11)) 
-        OR (a.zainum = b.zainum12)) 
-        OR (a.zainum = b.zainum13)) 
-        OR (a.zainum = b.zainum14)) 
-        OR (a.zainum = b.zainum15))) 
-     UNION ALL SELECT a.hospnum,
-           a.ptid,
-           '1' AS nyugaikbn,
-           b.sryka,
-           b.srykbn,
-           '   '::bpchar AS srysyukbn,
-           a.sryymd,
-           b.srycd AS srycd1,
-           b.srycd AS srycd2,
-           b.srycd AS srycd3,
-           b.srycd AS srycd4,
-           b.srycd AS srycd5 
-      FROM tbl_nrrksrh a,
-           tbl_nsrysrh b 
-     WHERE (((a.hospnum = b.hospnum) 
-       AND (a.ptid = b.ptid)) 
-       AND (a.zainum = b.zainum));
-
-COMMENT 
-        ON VIEW view_q002 IS '患者照会ビュー2';
-
 CREATE VIEW view_i001 AS
     SELECT a.hospnum,
            a.ptid,
@@ -660,4 +605,17 @@ CREATE VIEW view_bd002 AS
 
 COMMENT 
         ON VIEW view_bd002 IS '収納ビュー2';
+
+
+CREATE view view_q002 as
+SELECT b.hospnum, b.ptid, b.nyugaikbn, a.sryka, a.srykbn, a.srysyukbn, b.sryymd, a.srycd1, a.srycd2, a.srycd3, a.srycd4, a.srycd5
+   FROM tbl_sryact a, tbl_jyurrk b
+  WHERE a.hospnum  = b.hospnum::smallint AND a.ptid = b.ptid::bigint AND a.nyugaikbn = b.nyugaikbn AND a.sryka = b.sryka AND (a.zainum::numeric = b.zainum1 OR a.zainum::numeric = b.zainum2 OR a.zainum::numeric = b.zainum3 OR a.zainum::numeric = b.zainum4 OR a.zainum::numeric = b.zainum5 OR a.zainum::numeric = b.zainum6 OR a.zainum::numeric = b.zainum7 OR a.zainum::numeric = b.zainum8 OR a.zainum::numeric = b.zainum9 OR a.zainum::numeric = b.zainum10 OR a.zainum::numeric = b.zainum11 OR a.zainum::numeric = b.zainum12 OR a.zainum::numeric = b.zainum13 OR a.zainum::numeric = b.zainum14 OR a.zainum::numeric = b.zainum15)
+UNION ALL 
+ SELECT a.hospnum, a.ptid, '1' AS nyugaikbn, b.sryka, b.srykbn, '   '::bpchar AS srysyukbn, a.sryymd, b.srycd AS srycd1, b.srycd AS srycd2, b.srycd AS srycd3, b.srycd AS srycd4, b.srycd AS srycd5
+   FROM tbl_nrrksrh a, tbl_nsrysrh b
+  WHERE a.hospnum = b.hospnum AND a.ptid = b.ptid AND a.zainum = b.zainum;
+
+COMMENT 
+        ON VIEW view_q002 IS '患者照会ビュー2';
 
