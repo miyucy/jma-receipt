9192fea13e4fb6455526d74e634e8469f3606148
tatano <tatano@cvs.orca.med.or.jp>
2014-01-30 11:16:26 +0900

    介護保険認定情報設定対応

---
+++ sql/4.8/orcadbcr_ptcare_hkninf_4.8.0-1.sql
@@ -0,0 +1,20 @@
+--                                    --
+-- 患者介護保険情報テーブルの作成       --
+--                                    --
+-- Create Date : 2014/01/28           --
+--                                    --
+
+CREATE TABLE tbl_ptcare_hkninf (
+	HOSPNUM		smallint NOT NULL,
+	PTID		bigint,
+        HKNJANUM	character varying(6),
+        HIHKNJANUM	character varying(10),
+	TERMID		character varying(16),
+	OPID		character varying(16),
+	CREYMD		character varying(8),
+	UPYMD		character varying(8),
+	UPHMS		character varying(6)
+);
+
+ALTER TABLE ONLY tbl_ptcare_hkninf
+    ADD CONSTRAINT tbl_ptcare_hkninf_primary_key PRIMARY KEY (HOSPNUM,PTID);
