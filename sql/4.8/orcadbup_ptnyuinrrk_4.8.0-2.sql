d76d2a8577c6610c95b6085cf7cb336cac959542
azu <azu@cvs.orca.med.or.jp>
2014-04-23 11:54:18 +0900

    特定入院料の算定要件外対応

--- sql/4.8/orcadbup_ptnyuinrrk_4.8.0-2.sql
+++ sql/4.8/orcadbup_ptnyuinrrk_4.8.0-2.sql
@@ -1,4 +1,4 @@
 \set ON_ERROR_STOP
 
-alter table tbl_ptnyuinrrk add column toku_santeiyoukenkbn  char(1)  default '0';
-update tbl_ptnyuinrrk set toku_santeiyoukenkbn ='0' ;
+alter table tbl_ptnyuinrrk add column toku_santeiyoukenkbn  char(1)  default '';
+update tbl_ptnyuinrrk set toku_santeiyoukenkbn ='' ;
