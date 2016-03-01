82542330a10ffb7799c722ffa653139b4f4bbc23
tosihiko <tosihiko@cvs.orca.med.or.jp>
2016-03-01 15:10:55 +0900

    改定対応（点数マスタ新設項目）

---
+++ sql/4.9/orcadbup_tensu_4.9.0-1.sql
@@ -0,0 +1,15 @@
+--                                      
+-- 点数テーブル
+--                                    
+-- 非侵襲的血行動態モニタリング加算追加
+-- 凍結保存同種組織加算追加
+-- 点数表区分番号追加
+--                                   
+-- Create Date : 2016/03/01      
+
+\set ON_ERROR_STOP
+
+alter table tbl_tensu add column hisinsyumonitorksn smallint default 0;
+alter table tbl_tensu add column touketuhozonksn smallint default 0;
+alter table tbl_tensu add column kubunbangou character varying(30);
+update tbl_tensu set kubunbangou = '';
