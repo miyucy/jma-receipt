cf037f21b9957adf45c6f70c450b5f736fb27dd4
fujihara <fujihara@cvs.orca.med.or.jp>
2016-08-17 10:10:24 +0900

    災害区分長さ変更

--- sql/4.9/orcadbup_henrei_body_4.9.0-1.sql
+++ sql/4.9/orcadbup_henrei_body_4.9.0-1.sql
@@ -2,15 +2,15 @@
 
 \set ON_ERROR_STOP
 
-alter table tbl_henrei_body add column disaster char(1);
-alter table tbl_henrei_body add column s_disaster_1 char(1);
-alter table tbl_henrei_body add column s_disaster_2 char(1);
-alter table tbl_henrei_body add column s_disaster_3 char(1);
-alter table tbl_henrei_body add column s_disaster_4 char(1);
-alter table tbl_henrei_body add column s_disaster_5 char(1);
-alter table tbl_henrei_body add column s_disaster_6 char(1);
-alter table tbl_henrei_body add column s_disaster_7 char(1);
-alter table tbl_henrei_body add column s_disaster_8 char(1);
-alter table tbl_henrei_body add column s_disaster_9 char(1);
-alter table tbl_henrei_body add column s_disaster_10 char(1);
-update tbl_henrei_body set disaster='0',s_disaster_1='',s_disaster_2='',s_disaster_3='',s_disaster_4='',s_disaster_5='',s_disaster_6='',s_disaster_7='',s_disaster_8='',s_disaster_9='',s_disaster_10='';
+alter table tbl_henrei_body add column disaster char(2);
+alter table tbl_henrei_body add column s_disaster_1 char(2);
+alter table tbl_henrei_body add column s_disaster_2 char(2);
+alter table tbl_henrei_body add column s_disaster_3 char(2);
+alter table tbl_henrei_body add column s_disaster_4 char(2);
+alter table tbl_henrei_body add column s_disaster_5 char(2);
+alter table tbl_henrei_body add column s_disaster_6 char(2);
+alter table tbl_henrei_body add column s_disaster_7 char(2);
+alter table tbl_henrei_body add column s_disaster_8 char(2);
+alter table tbl_henrei_body add column s_disaster_9 char(2);
+alter table tbl_henrei_body add column s_disaster_10 char(2);
+update tbl_henrei_body set disaster='00',s_disaster_1='',s_disaster_2='',s_disaster_3='',s_disaster_4='',s_disaster_5='',s_disaster_6='',s_disaster_7='',s_disaster_8='',s_disaster_9='',s_disaster_10='';
