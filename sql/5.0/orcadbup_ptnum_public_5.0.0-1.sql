8b8e3607d68277a267868dc6a01743a97b5e0cef
morirei <morirei@cvs.orca.med.or.jp>
2016-10-24 12:01:44 +0900

    PTNUM-PUBLIC ADD PATIENTID2 PATIENTID3

---
+++ sql/5.0/orcadbup_ptnum_public_5.0.0-1.sql
@@ -0,0 +1,14 @@
+--                                      
+-- 患者地域連携テーブル
+--                                    
+-- 医療等ＩＤ追加
+-- 共通番号追加
+--                                   
+-- Create Date : 2016/10/24      
+
+\set ON_ERROR_STOP
+
+alter table tbl_ptnum_public add column patient_id_2 character(20);
+alter table tbl_ptnum_public add column patient_id_3 character(20);
+update tbl_ptnum_public set patient_id_2 = '';
+update tbl_ptnum_public set patient_id_3 = '';
