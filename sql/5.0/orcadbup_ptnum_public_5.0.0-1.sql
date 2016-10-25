c463ecd0befe380f0541fbaf0e6a9293713cbe77
tosihiko <tosihiko@cvs.orca.med.or.jp>
2016-10-25 13:50:02 +0900

    個人番号タイプ変更

--- sql/5.0/orcadbup_ptnum_public_5.0.0-1.sql
+++ sql/5.0/orcadbup_ptnum_public_5.0.0-1.sql
@@ -8,7 +8,7 @@
 
 \set ON_ERROR_STOP
 
-alter table tbl_ptnum_public add column patient_id_2 character(20);
-alter table tbl_ptnum_public add column patient_id_3 character(20);
+alter table tbl_ptnum_public add column patient_id_2 character varying(20);
+alter table tbl_ptnum_public add column patient_id_3 character varying(20);
 update tbl_ptnum_public set patient_id_2 = '';
 update tbl_ptnum_public set patient_id_3 = '';
