09630d25648a416016d5efde0901840b301d747e
tosihiko <tosihiko@cvs.orca.med.or.jp>
2016-11-30 17:21:15 +0900

    マスター系テーブルの分離（その３）医療機関識別番号のないテーブル

--- sql/5.0/orcadbcr_schema-master_5.0.0-1.sql
+++ sql/5.0/orcadbcr_schema-master_5.0.0-1.sql
@@ -55,34 +55,51 @@ CREATE VIEW tbl_nyuinryakusho AS SELECT * FROM master.tbl_nyuinryakusho;
 ALTER TABLE IF EXISTS tbl_synonym SET SCHEMA master;
 CREATE VIEW tbl_synonym AS SELECT * FROM master.tbl_synonym;
 
+-- 電子点数表
+ALTER TABLE IF EXISTS tbl_etensu_1 SET SCHEMA master;
+CREATE VIEW tbl_etensu_1 AS SELECT * FROM master.tbl_etensu_1;
+ALTER TABLE IF EXISTS tbl_etensu_2 SET SCHEMA master;
+CREATE VIEW tbl_etensu_2 AS SELECT * FROM master.tbl_etensu_2;
+ALTER TABLE IF EXISTS tbl_etensu_2_jma SET SCHEMA master;
+CREATE VIEW tbl_etensu_2_jma AS SELECT * FROM master.tbl_etensu_2_jma;
+ALTER TABLE IF EXISTS tbl_etensu_2_sample SET SCHEMA master;
+CREATE VIEW tbl_etensu_2_sample AS SELECT * FROM master.tbl_etensu_2_sample;
+ALTER TABLE IF EXISTS tbl_etensu_3_1 SET SCHEMA master;
+CREATE VIEW tbl_etensu_3_1 AS SELECT * FROM master.tbl_etensu_3_1;
+ALTER TABLE IF EXISTS tbl_etensu_3_2 SET SCHEMA master;
+CREATE VIEW tbl_etensu_3_2 AS SELECT * FROM master.tbl_etensu_3_2;
+ALTER TABLE IF EXISTS tbl_etensu_3_3 SET SCHEMA master;
+CREATE VIEW tbl_etensu_3_3 AS SELECT * FROM master.tbl_etensu_3_3;
+ALTER TABLE IF EXISTS tbl_etensu_3_4 SET SCHEMA master;
+CREATE VIEW tbl_etensu_3_4 AS SELECT * FROM master.tbl_etensu_3_4;
+ALTER TABLE IF EXISTS tbl_etensu_4 SET SCHEMA master;
+CREATE VIEW tbl_etensu_4 AS SELECT * FROM master.tbl_etensu_4;
+ALTER TABLE IF EXISTS tbl_etensu_5 SET SCHEMA master;
+CREATE VIEW tbl_etensu_5 AS SELECT * FROM master.tbl_etensu_5;
 
--- -- 電子点数表
--- ALTER TABLE IF EXISTS tbl_etensu_1 SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_etensu_2 SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_etensu_2_jma SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_etensu_2_sample SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_etensu_3_1 SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_etensu_3_2 SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_etensu_3_3 SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_etensu_3_4 SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_etensu_4 SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_etensu_5 SET SCHEMA master;
-
-
--- -- 労働基準監督署
--- ALTER TABLE IF EXISTS tbl_labor_sio SET SCHEMA master;
--- -- 最低薬価
--- ALTER TABLE IF EXISTS tbl_generic_price SET SCHEMA master;
--- -- 介護保険者
--- ALTER TABLE IF EXISTS tbl_hknjainf_care SET SCHEMA master;
--- -- 選定療養費一般コード振替
--- ALTER TABLE IF EXISTS tbl_senteicdchg SET SCHEMA master;
--- -- 入院料置換
--- ALTER TABLE IF EXISTS tbl_nyuincdchg SET SCHEMA master;
--- -- 医薬品傷病名
--- ALTER TABLE IF EXISTS tbl_tekiou_medicine SET SCHEMA master;
--- ALTER TABLE IF EXISTS tbl_tekiou_disease SET SCHEMA master;
--- -- 医薬品分類
--- ALTER TABLE IF EXISTS tbl_generic_class SET SCHEMA master;
--- -- 向精神薬成分コード
--- ALTER TABLE IF EXISTS tbl_psychotropic SET SCHEMA master;
+-- 労働基準監督署
+ALTER TABLE IF EXISTS tbl_labor_sio SET SCHEMA master;
+CREATE VIEW tbl_labor_sio AS SELECT * FROM master.tbl_labor_sio;
+-- 最低薬価
+ALTER TABLE IF EXISTS tbl_generic_price SET SCHEMA master;
+CREATE VIEW tbl_generic_price AS SELECT * FROM master.tbl_generic_price;
+-- 介護保険者
+ALTER TABLE IF EXISTS tbl_hknjainf_care SET SCHEMA master;
+CREATE VIEW tbl_hknjainf_care AS SELECT * FROM master.tbl_hknjainf_care;
+-- 選定療養費一般コード振替
+ALTER TABLE IF EXISTS tbl_senteicdchg SET SCHEMA master;
+CREATE VIEW tbl_senteicdchg AS SELECT * FROM master.tbl_senteicdchg;
+-- 入院料置換
+ALTER TABLE IF EXISTS tbl_nyuincdchg SET SCHEMA master;
+CREATE VIEW tbl_nyuincdchg AS SELECT * FROM master.tbl_nyuincdchg;
+-- 医薬品傷病名
+ALTER TABLE IF EXISTS tbl_tekiou_medicine SET SCHEMA master;
+CREATE VIEW tbl_tekiou_medicine AS SELECT * FROM master.tbl_tekiou_medicine;
+ALTER TABLE IF EXISTS tbl_tekiou_disease SET SCHEMA master;
+CREATE VIEW tbl_tekiou_disease AS SELECT * FROM master.tbl_tekiou_disease;
+-- 医薬品分類
+ALTER TABLE IF EXISTS tbl_generic_class SET SCHEMA master;
+CREATE VIEW tbl_generic_class AS SELECT * FROM master.tbl_generic_class;
+-- 向精神薬成分コード
+ALTER TABLE IF EXISTS tbl_psychotropic SET SCHEMA master;
+CREATE VIEW tbl_psychotropic AS SELECT * FROM master.tbl_psychotropic;
